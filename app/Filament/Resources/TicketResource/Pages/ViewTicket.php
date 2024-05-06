777fec6f5aab7b77026cf5b13b5c5a286890491d
halilcosdu <halilcosdu@users.noreply.github.com>
2024-05-06 17:45:28 +0000

    Fix styling

--- app/Filament/Resources/TicketResource/Pages/ViewTicket.php
+++ app/Filament/Resources/TicketResource/Pages/ViewTicket.php
@@ -15,7 +15,7 @@ class ViewTicket extends ViewRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\EditAction::make()->slideOver()
+            Actions\EditAction::make()->slideOver(),
         ];
     }
 }
