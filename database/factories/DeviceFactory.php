a8aa73dfc176e57fbc6d9e7d48c0045083b3fbc3
halilcosdu <halilcosdu@users.noreply.github.com>
2024-05-06 18:13:21 +0000

    Fix styling

--- database/factories/DeviceFactory.php
+++ database/factories/DeviceFactory.php
@@ -13,7 +13,7 @@ class DeviceFactory extends Factory
      * Define the model's default state.
      *
      * @return array<string, mixed>
- */
+     */
     public function definition(): array
     {
         return [
